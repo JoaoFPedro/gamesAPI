197d486e687da77be560113568d8ab773a192961
Joao Pedro <joao.fpsilva98@gmail.com>
2024-06-21 20:39:04 -0300

    creating game repository

---
+++ src/repositories/gameRepository.ts
@@ -0,0 +1,16 @@
+// src/repositories/gamesRepository.ts
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
+const filePath = join(__dirname, '../data/games.json');
+
+export const getGamesFromRepository = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const data = await fs.readFile(filePath, 'utf-8');
+            resolve(JSON.parse(data));
+        } catch (error) {
+            reject(new Error('Could not read games data'));
+        }
+    });
+};
